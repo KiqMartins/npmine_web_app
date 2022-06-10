487b270d557d816148f33bae5dd4cd8ddf9a9510
aclcoelho <ana.lunardello@gmail.com>
2022-06-10 16:47:18 -0300

    Adding PostgreSQL and environment variables

--- websiteNPMINE/__init__.py
+++ websiteNPMINE/__init__.py
@@ -2,15 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "test_np"
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    user = os.environ['DB_USERNAME']
+    password = os.environ['DB_PASSWORD']
 
     app.config['SECRET_KEY'] = 'fbfhdfbsdkifbsuf'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@localhost/{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
@@ -20,7 +23,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+    from .models import Accounts
 
     create_database(app)
 
@@ -30,7 +33,7 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(id):
-        return User.query.get(int(id))
+        return Accounts.query.get(int(id))
 
     return app
 
