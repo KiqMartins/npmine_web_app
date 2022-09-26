0f65f6e0ce7aeda7fe044f694a22c630738138b6
aclcoelho <ana.lunardello@gmail.com>
2022-09-26 15:11:08 -0300

    Altered add new compound and register verification

--- websiteNPMINE/__init__.py
+++ websiteNPMINE/__init__.py
@@ -6,6 +6,7 @@ import os
 from websiteNPMINE.config import Config
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 
@@ -13,21 +14,23 @@ bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'main.home'
 login_manager.login_message_category = 'info'
+bootstrap = Bootstrap()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
     bcrypt.init_app(app)
     db.init_app(app)
+    bootstrap.init_app(app)
 
     login_manager.init_app(app)
 
     from websiteNPMINE.users.routes import users
     from websiteNPMINE.main.routes import main
-    from websiteNPMINE.inserts.routes import inserts
+    from websiteNPMINE.compounds.routes import compounds
     app.register_blueprint(users)
     app.register_blueprint(main)
-    app.register_blueprint(inserts)
+    app.register_blueprint(compounds)
 
     migrate = Migrate(app,db)
 
