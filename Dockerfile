af0673b1e2fc572f99aa2d2b28a72a25c4801f12
aclcoelho <ana.lunardello@gmail.com>
2023-06-28 17:14:46 -0300

    Added database and individual compound pages

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,14 @@ RUN conda env create -f environment.yml
 RUN echo "source activate npmine_web_app" > ~/.bashrc
 ENV PATH /opt/conda/envs/npmine_web_app/bin:$PATH
 
+# Install PostgreSQL client
+RUN apt-get update && apt-get install -y postgresql-client
+
 # Copy the application code to the container
-COPY . .
+COPY . /app
+
+# Set permissions for the init_db.sh script
+RUN chmod +x /app/init_db.sh
 
 # Expose the port on which the Flask app will run
 EXPOSE 5000
@@ -24,6 +30,5 @@ ENV FLASK_ENV=production
 
 # Command to run the Flask app
 CMD ["flask", "run", "--host=0.0.0.0"]
-
 
 
--- websiteNPMINE/main/routes.py
+++ websiteNPMINE/main/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request
-from websiteNPMINE.models import Compounds, Accounts
+from flask import Blueprint, render_template, request,jsonify, abort
+from websiteNPMINE.models import Compounds, Accounts, doicomp, DOI
 from websiteNPMINE import db
 
 main = Blueprint('main', __name__)
@@ -11,42 +11,59 @@ def home():
 
 @main.route('/api/data')
 def data():
+    # Pagination parameters
+    start = request.args.get('start', type=int, default=0)
+    length = request.args.get('length', type=int, default=10)
+
+    # Search filter
+    search = request.args.get('search', '').strip()
+    search_query = db.or_(
+        Compounds.id.like(f'%{search}%'),
+        Compounds.smiles.like(f'%{search}%')
+    )
+
+    # Sorting
+    sort = request.args.get('sort', '')
+    sort_columns = ['id', 'inchikey', 'pubchem']
+    order = []
+    for field in sort.split(','):
+        direction = '-' if field.startswith('-') else ''
+        name = field.lstrip('-')
+        if name not in sort_columns:
+            name = 'id'
+        col = getattr(Compounds, name)
+        order.append(getattr(col, direction + 'asc')())
+    if not order:
+        order.append(Compounds.id.asc())
+
+    # Fetch data
     query = Compounds.query
-
-    # search filter
-    search = request.args.get('search')
     if search:
-        query = query.filter(db.or_(
-            Compounds.id_comp.like(f'%{search}%'),
-            Compounds.smiles.like(f'%{search}%')
-        ))
+        query = query.filter(search_query)
     total = query.count()
+    query = query.order_by(*order).offset(start).limit(length)
+    compounds = query.all()
 
-    # sorting
-    sort = request.args.get('sort')
-    if sort:
-        order = []
-        for s in sort.split(','):
-            direction = s[0]
-            name = s[1:]
-            if name not in ['id_comp', 'smiles', 'inchikey', 'pubchem']:
-                name = 'id_comp'
-            col = getattr(Compounds, name)
-            if direction == '-':
-                col = col.desc()
-            order.append(col)
-        if order:
-            query = query.order_by(*order)
+    # Prepare response data
+    data = []
+    for compound in compounds:
+        compound_data = compound.to_dict()
+        compound_data['id'] = compound.id
+        data.append(compound_data)
 
-    # pagination
-    start = request.args.get('start', type=int, default=-1)
-    length = request.args.get('length', type=int, default=-1)
-    if start != -1 and length != -1:
-        query = query.offset(start).limit(length)
+    return jsonify({'data': data, 'total': total})
 
-    # response
-    return {
-        'data': [compounds.to_dict() for compounds in query],
-        'total': total,
-    }
+@main.route('/compound/<int:compound_id>')
+def compound(compound_id):
+    compound = Compounds.query.get(compound_id)
+    if not compound:
+        abort(404)
 
+    # Fetch the required information from the compound object
+    compound_id = compound.id
+    inchi = compound.inchi
+    inchi_key = compound.inchi_key
+    pubchem_id = compound.pubchem_id
+    exact_mol_weight = compound.exact_molecular_weight
+
+    return render_template('compound.html', compound_id=compound_id, inchi=inchi, inchi_key=inchi_key, pubchem_id=pubchem_id, exact_mol_weight=exact_mol_weight)
